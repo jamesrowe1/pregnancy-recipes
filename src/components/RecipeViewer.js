cb1338aba77c2d5f7b5f0c010f0df7396081284a
James Rowe <jamesrowe@outlook.com>
2025-07-10 17:53:56 -0400

    fixed mapping

--- src/components/RecipeViewer.js
+++ src/components/RecipeViewer.js
@@ -5,7 +5,10 @@ function RecipeViewer() {
   const { recipeName } = useParams();
 
   const fileMap = {
+    "30-minute-chicken-pasta-primavera": "30-Minute Chicken Pasta Primavera.html",
     "arugula-salad-with-fruit": "Arugula Salad With Fruit.html",
+    "bibimbap": "Bibimbap.html",
+    "corn-salad-without-mayo": "Corn Salad Without Mayo.html",
     "couscous-salad-with-butternut-squash-and-cranberries": "Couscous Salad with Butternut Squash and Cranberries.html",
     "garlic-parmesan-kale-pasta": "Garlic Parmesan Kale Pasta.html",
     "grilled-sweet-potatoes": "Grilled Sweet Potatoes.html",
@@ -17,12 +20,14 @@ function RecipeViewer() {
     "rotini-with-chicken-and-garden-vegetables": "Rotini with Chicken and Garden Vegetables.html",
     "rub-lamb-chops": "Rub Lamb Chops.html",
     "salmon-couscous-salad-bowl": "Salmon Couscous Salad Bowl.html",
+    "slow-cooker-mongolian-beef": "Slow Cooker Mongolian Beef.html",
     "slow-cooker-lamb-curry": "Slow Cooker Lamb Curry.html",
     "sous-vide-turkey-breast": "Sous Vide Turkey Breast.html",
     "steak-fajitas-with-peppers-onions": "Steak Fajitas with Peppers & Onions.html",
     "sweet-potato-bowl": "Sweet Potato Bowl.html",
     "taste-of-morocco": "Taste of Morocco.html",
     "tuscan-style-veal-chops": "Tuscan-Style Veal Chops.html",
+    "watermelon-salad-recipe-with-feta-and-cucumber": "Watermelon Salad Recipe with Feta and Cucumber.html",
     "yellow-curry-with-chicken-kale-butternut-squash": "Yellow Curry with Chicken, Kale & Butternut Squash.html"
   };
 
